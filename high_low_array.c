8bfe7777d214846179fa5a06970c6870825e4fca
Thirunethra <umamaheswari.thirunethra@gmail.com>
2016-07-06 12:59:04 +0530

    Create high_low_array.c

---
+++ high_low_array.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+  int arr[100],max,min,i,n;
+  scanf("%d\n",&n);
+  for(i=0;i<n;i++)
+    scanf("%d",&arr[i]);
+  min=arr[0];
+  for(i=1;i<n;i++)
+  {
+    if(min>arr[i])
+      min=arr[i];
+  }
+  max=arr[0];
+  for(i=1;i<n;i++)
+  {
+    if(arr[i]>max)
+      max=arr[i];
+  }
+  printf("%d%d",max,min);
+  return 0;
+}

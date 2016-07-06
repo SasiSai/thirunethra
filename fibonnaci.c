34d3558ab02ef18669324a161e6025b1cff08c5d
Thirunethra <umamaheswari.thirunethra@gmail.com>
2016-07-06 10:02:07 +0530

    Create fibonnaci.c

---
+++ fibonnaci.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+  int num,c,a=0,b=1,i;
+  printf("%d %d",a,b);
+  for(i=0;i<num-2;i++)
+  {
+    c=a+b;
+    a=b;
+    b=c;
+    printf("%d",c);
+  }
+  return 0;
+}

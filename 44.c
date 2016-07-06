ae62bf8f976fbbbfd6f5869c4a876d929cc613c1
Thirunethra <umamaheswari.thirunethra@gmail.com>
2016-07-06 13:04:01 +0530

    Create 44.c

---
+++ 44.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+  int num;
+  scanf("%d",&num);
+  if(num==0 && num==9)
+    printf("%d is in range",num);
+  else
+    printf("You entered number is not in range");
+  return 0;
+  }

46fd6a98a30b2e10083f8a6c7c51e0f08026da65
Thirunethra <umamaheswari.thirunethra@gmail.com>
2016-07-07 06:06:17 +0530

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -3,9 +3,14 @@ int main()
 {
   int  num,f=1,i;
   scanf("%d",&num);
+  if(num==0)
+    f=1;
+  else
+  {
   for(i=1;i<num;i++)
   {
     f=f*i;
+  }
   }
   printf("%d is factorial of %d",f,num);
   return 0;

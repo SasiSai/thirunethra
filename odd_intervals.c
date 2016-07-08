21f8977c24bc596a37ba750c62a3e94ac85a9fb2
Thirunethra <umamaheswari.thirunethra@gmail.com>
2016-07-08 21:25:18 +0530

    Create odd_intervals.c

---
+++ odd_intervals.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+  int min,max,i;
+  scanf("%d%d",&min,&max);
+  if(min%2==0)
+    min=min+1;
+  for(i=min;i<=max;i+=2)
+  {
+    printf("%d",i);
+  }
+  return 0;
+}

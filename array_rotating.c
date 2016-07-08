6a0492785cc0cb8c303cc5ac0876b007b5770bc2
Thirunethra <umamaheswari.thirunethra@gmail.com>
2016-07-08 14:13:18 +0530

    Create array_rotating.c

---
+++ array_rotating.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+  int arr[100],i,k,diff,n;
+  scanf("%d\n",&n);
+  for(i=0;i<n;i++)
+  {
+    scanf("%d",&arr[i]);
+  }
+  scanf("\n%d",&k);
+  diff=n-k;
+  for(i=diff;i<n;i++)
+  {
+    printf("%d",arr[i]);
+  }
+  for(i=0;i<diff;i++)
+  {
+    printf("%d",arr[i]);
+  }
+  return 0;
+}

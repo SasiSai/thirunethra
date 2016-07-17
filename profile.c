a327eb1892108bdc71cf5620fb34612a96f53250
Thirunethra <umamaheswari.thirunethra@gmail.com>
2016-07-17 07:53:32 +0530

    Create profile.c

---
+++ profile.c
@@ -0,0 +1,103 @@
+<html>
+<title>Thirunethra_Profile</title>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="I:/Bootstrap/bootstrap-3.3.6-dist/css/bootstrap.min.css" rel="stylesheet">
+<style type="text/css">
+h3{
+font-family:"Verdana";
+font-size:19px;
+text-decoration-style:underline;
+}
+blockquote {
+  background: #f9f9f9;
+  border-left: 10px solid #ccc;
+  margin: 1.5em 10px;
+  padding: 0.5em 10px;
+}
+</style>
+<h1 align=CENTER>RESUME</h1>
+</head>
+<body>
+<h3>ABOUT</h3>
+<p><blockquote style="color:black">Name       :-<em>R.THIRUNETHRA</em><br/>
+Degree        :-<b>Bachelor of Engineering in Computer science and Engineering</b><br/>
+
+Contact Number:- <b>9095771503</b><br/>
+E-Mail ID     :- <b>thirunethra@hotmail.com</b><br/>
+Present Address:-<b>D/o,K.Rajeswaran,</b>
+                  <b>7/4,Park street(TK),<br/>
+                  Mallur,Salem-636203.</b></blockquote></p>
+<h3>CAREER OBJECTIVE</h3>
+<p>To work in an environment where i can compile my technical knowledge which offers professional advancement and financial heights.</p>
+<h3>ACADEMIC PROFILE</h3>
+<table border=1,align:center>
+<caption>Educational Qualifications</caption>
+<tr>
+<th><b>Qualification</b></th>
+<th><b>Board/University</b></th>
+<th><b>Percentage</b></th>
+<th><b>Year passed out</b></th>
+</tr>
+<tr>
+<td>10<sup>th</sup></td>
+<td>Matriculation</td>
+<td>94.4%</td>
+<td>2011</td>
+</tr>
+<tr>
+<td>12<sup>th</sup></td>
+<td>State Board</td>
+<td>95%</td>
+<td>2013</td>
+</tr>
+<tr>
+<td>Bachelor of Engineering in Computer Science and Engineering</td>
+<td>Bannari Amman Institute of Technology</td>
+<td>90.8%</td>
+<td>2017</td>
+</tr>
+</table>
+<h3>MINI PROJECT</h3>
+<table border=1>
+<tr>
+<td>Title</td>
+<td>Online Gas Booking System</td>
+</tr>
+<tr>
+<td>Period</td>
+<td>1 Month</td>
+</tr>
+<tr>
+<td>Technology Used</td>
+<td>C#</td>
+</tr>
+<tr>
+<td>My Role</td>
+<td>Development of Application</td>
+</tr>
+</table>
+
+<h3>TEAM MEMBERS</h3>
+<p>The Mini Project was done by team of two</p>
+
+<h3>IT PROFIECIENCY</h3>
+<p><li>C<li>Python<li>Java</p>
+<h3>PROJECTS</h3>
+<p>Intelligent Automatic Plant Irrigation System.</p>
+<h3>CO-CURRICULAR ACTIVITIES</h3>
+<p>Event Co-coordinator for Registration Committee.</p>
+
+<h3>INTERPERSONAL SKILLS</h3>
+<p><li>Quick observer,<li>Hard and Consistent worker</p>
+
+<h3>PERSONAL DETAILS</h3>
+<p><li>Father's Name-<b>K.Rajeswaran</b><li>Mother's Name-<b>R.Pramiladevi</b><li>DOB-<b>26/08/1996</b><li>salem-636203.<li>Contact Number-9095771503<li>NATIONALITY-<b>INDIAN</b></p>
+
+<h3>DECLARATION</h3>
+<p>The above Furnished Information are true and complete to the Best of my Knowledge.</p>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="I:/Bootstrap/bootstrap-3.3.6-dist/js/bootstrap.min.js"></script>
+</body>
+</html>
